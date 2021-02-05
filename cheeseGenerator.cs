a10c5cbdfa13b46c7a05edb8f74c9e113694ed9b
Melany Estevez Camata <63152500+Mademoisellefou@users.noreply.github.com>
2021-02-05 07:02:58 -0400

    Add files via upload
    basic_moves

---
+++ cheeseGenerator.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cheeseGenerator : MonoBehaviour
+{
+
+
+    //States
+    public enum SpawerState
+    {
+        SPAWING, WAITING, COUNTING, END
+    }
+    [System.Serializable]
+
+    //Create a object
+    public class Wave
+    {
+        //name
+        public string Name;
+        public Transform enemy;  
+    }
+    //Waves
+    public Wave[] waves;
+    int MaxNextWave;
+    private int NextWave = 0;
+    public float Diference = 5f;
+    public float CountDownWave;
+    public SpawerState _state = SpawerState.COUNTING;
+    public float SearchCountDown = 1f;
+    public Transform[] positions;
+
+    private void Start()
+    {
+        CountDownWave = 0;
+        MaxNextWave = waves.Length;
+
+    }
+
+    public bool IsAlive()
+    {
+        SearchCountDown -= Time.deltaTime;
+        if (SearchCountDown <= 0)
+        {
+            SearchCountDown = 1f;
+            if (GameObject.FindGameObjectWithTag("Cheese") == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public void NextWaveSpaw()
+    {
+        _state = SpawerState.COUNTING;
+        if (NextWave + 1 == MaxNextWave)
+        {
+            _state = SpawerState.END;
+
+        }
+        else
+        {
+            NextWave++;
+        }
+        CountDownWave = Diference;
+    }
+    private void Update()
+    {
+        if (SpawerState.END == _state) return;
+        if (SpawerState.WAITING == _state)
+        {
+            if (!IsAlive())
+            {
+                NextWaveSpaw();
+                //Debug.Log("Creating ...");
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+        if (CountDownWave <= 0)
+        {
+            if (!_state.Equals(SpawerState.SPAWING))
+            {
+
+                StartCoroutine(StartSpaw(waves[NextWave]));
+
+            }
+        }
+        else
+        {
+            CountDownWave -= Time.deltaTime;
+        }
+    }
+    IEnumerator StartSpaw(Wave w)
+    {
+
+        _state = SpawerState.SPAWING;
+        SpawEnemy(w.enemy);
+        yield return new WaitForSeconds(1f);
+ 
+        _state = SpawerState.WAITING;
+
+        yield break;
+    }
+    void SpawEnemy(Transform pos)
+    {
+        var posRandom = positions[NextWave];
+        Instantiate(pos, posRandom.position, Quaternion.identity);
+        //Debug.Log(pos);
+    }
+
+
+
+
+}
---
+++ towndownmove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class towndownmove : MonoBehaviour
+{
+    [Header("Player")]
+    SpriteRenderer sr;
+    public float moveSpeed=5f;
+    Rigidbody2D rb;
+    Vector2 mov;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        mov.x = Input.GetAxisRaw("Horizontal");
+        mov.y = Input.GetAxisRaw("Vertical");
+        //
+        anim.SetFloat("h",mov.x);
+        anim.SetFloat("v", mov.y);
+        anim.SetFloat("speed", mov.sqrMagnitude);
+        //animations here
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+mov*moveSpeed);
+    }
+}
