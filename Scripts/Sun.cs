9e1d480da5aca5062bc314bdafaf4aeaaa7418e3
Melany Estevez Camata <63152500+Mademoisellefou@users.noreply.github.com>
2021-02-05 07:04:20 -0400

    Add files via upload
    basic moves 2d

---
+++ Scripts/Sun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Sun : MonoBehaviour
+{
+    public Light2D _l;
+    [SerializeField]
+    public float _speedL;
+    //ligth will turn:)using UnityEngine.Experimental.Rendering.Universal;
+    private void Start()
+    {
+
+        StartCoroutine(sunset());
+    }
+    IEnumerator sunset()
+    {
+        while (_l.intensity > .5f)
+        {
+            _l.intensity -= _speedL;
+            yield return new WaitForSeconds(1f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(Sunrise());
+        StopCoroutine(sunset());
+    }
+    IEnumerator Sunrise()
+    {
+        while (_l.intensity < 1f)
+        {
+            _l.intensity += _speedL;
+            yield return new WaitForSeconds(1f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(sunset());
+        StopCoroutine(Sunrise());
+    }
+}
+  
---
+++ Scripts/sliderhealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderhealth : MonoBehaviour
+{
+
+    public Slider slider;
+    private void Awake()
+    {
+        slider.value = slider.maxValue;
+    }
+    public void step()
+    {
+        if (slider.value <= 0) return;
+        slider.value -=1;
+    }
+}
